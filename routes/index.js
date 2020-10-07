5d11a2e0a9d1ca3305855be5916a387d46de4d2d
Peter Oboukhovski <petero@mangodsp.com>
2020-10-07 20:02:19 +0300

    coundown established

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
+const launchTime= new Date('01/01/2021 00:00').getTime();
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  const currentTime = new Date().getTime();
+  const numberofMilliseconds = parseInt(launchTime-currentTime);
+  res.render(
+    'index', 
+    { title: 'Awesome game', countDown: numberofMilliseconds });
 });
 
 module.exports = router;
